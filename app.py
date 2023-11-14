63d2c51b5d20778b348cfa96efd066ae796b481d
thesupersystem <systm.mgck@gmail.com>
2023-11-14 17:43:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ vid_obj_detect = VideoObjectDetection()
 
 vid_obj_detect.setModelTypeAsYOLOv3()
 
-vid_obj_detect.setModelPath(r"C:/Users/Hogan/Documents/Research/objD/yolov3.pt")
+vid_obj_detect.setModelPath(r"C:/Users/YourDirectory/yolov3.pt")
 vid_obj_detect.loadModel()
 
+#Change to your directory
 detected_vid_obj = vid_obj_detect.detectObjectsFromVideo(
     input_file_path =  r"C:/Users/Hogan/Documents/Research/objD/input_video.mp4",
    output_file_path = r"C:/Users/Hogan/Documents/Research/objD/output_video",
